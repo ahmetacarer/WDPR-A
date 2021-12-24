c7ae13d1e2509d8c2d2343e6c36834add82d5c96
AhmetAcarer <20119046@student.hhs.nl>
2021-12-24 18:22:41 +0100

    Generate class aangemaakt als viewmodel

---
+++ src/ViewModels/Generate.cs
@@ -0,0 +1,16 @@
+
+namespace WDPR_A.ViewModels;
+
+public class Generate
+{
+    private readonly Random _random;
+
+    public Generate(Random random) // dependency injection through constructor
+    {
+        _random = random;
+    }
+
+    
+
+
+}

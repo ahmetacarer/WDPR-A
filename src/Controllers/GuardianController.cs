07793aa6b9717c498392986fc58fa842ba2c8579
Jamal <20107722@student.hhs.nl>
2021-12-26 21:53:27 +0100

    Accordion en opmaak toegevoegd in de view van de Dashboard

--- src/Controllers/GuardianController.cs
+++ src/Controllers/GuardianController.cs
@@ -8,16 +8,27 @@ namespace WDPR_A.Controllers;
 // [Authorize(Roles = "Guardian")]
 public class GuardianController : Controller
 {
+    private readonly WDPRContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public GuardianController(ILogger<HomeController> logger)
+    public GuardianController(ILogger<HomeController> logger, WDPRContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Dashboard()
     {
-        return View();
+        Guardian guardian = new Guardian()
+        {
+            FirstName = "Willem",
+            LastName = "Bommel",
+            Clients = new List<Client> {
+        new Client() { FirstName = "Erik", LastName = "Bommel", Condition = "ADHD" },
+        new Client() { FirstName = "Jordy", LastName = "Bommel", Condition = "Autisme" }
+        }
+        };
+        return View(guardian);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

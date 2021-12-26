88b650ec03b1b9f69ac578a29654d15b6d0e789d
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2021-12-26 16:28:27 +0100

    Footer gefixt Ahsan commit
    Footer gefixt Ahsan commit

---
+++ src/Controllers/AfspraakController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WDPR_A.Models;
+
+namespace WDPR_A.Controllers;
+
+public class AfspraakController : Controller
+{
+    private readonly ILogger<AfspraakController> _logger;
+
+    public AfspraakController(ILogger<AfspraakController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- src/Models/Appointment.cs
+++ src/Models/Appointment.cs
@@ -6,13 +6,17 @@ namespace WDPR_A.Models
     {
         public int Id { get; set; }
         public DateTime AppointmentDate { get; set; }
+
         [NotMapped]
         [Required]
         public Client? IncomingClient { get; set; }
+
         [NotMapped]
         public IList<Guardian>? Guardians { get; set; }
+
         [Required]
         public string? OrthopedagogueId { get; set; }
+        
         [Required]
         public Orthopedagogue Orthopedagogue { get; set; }
     }

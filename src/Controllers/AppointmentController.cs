78c00da8638b832c056161f23c220cf1c7720b2c
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2022-01-10 17:21:38 +0100

    MailHog works now

--- src/Controllers/AppointmentController.cs
+++ src/Controllers/AppointmentController.cs
@@ -28,6 +28,9 @@ public class AppointmentController : Controller
     {
         // tijdelijk
         // uitbreidbaar naar orthopedagoog met minste appointments
+        client.FirstName = MakeFirstNameCapitalLetter(client.FirstName);
+        client.LastName = client.LastName.ToLower();
+
         if (emailOfParent != null)
             client.Guardians = new List<Guardian>() { new Guardian { Email = emailOfParent } };
         var orthopedagogue = _context.Orthopedagogues.FirstOrDefault(o => o.Specialty == client.Condition);
@@ -41,7 +44,13 @@ public class AppointmentController : Controller
             Orthopedagogue = orthopedagogue,
             OrthopedagogueId = orthopedagogue.Id,
         };
-        System.Console.WriteLine(client.FirstName + client.LastName);
+
+        EmailTest(client.Email, appointmentDate, false);
+        if (emailOfParent != null) {
+             EmailTest(emailOfParent, appointmentDate, true);
+        }
+
+        System.Console.WriteLine(client.FirstName + " " + client.LastName);
 
         _context.Appointments.Add(appointment);
         _context.SaveChanges();
@@ -62,6 +71,66 @@ public class AppointmentController : Controller
         return View(); 
     }
 
+    public void EmailTest (string receiverEmail, DateTime AppointmentDate, bool isParent) {
+        //AppointmentDate.ToString("MM/dd/yyyy hh:mm");
+        string datum = AppointmentDate.ToString("MM/dd/yyyy");
+        Console.WriteLine(datum);
+        string tijd = AppointmentDate.ToString("HH:mm");
+        Console.WriteLine("-------------------------------------------------------");
+        Console.WriteLine(tijd);
+
+         //execute powershell cmdlets or scripts using command arguments as process
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = @"powershell.exe";
+        //execute powershell script using script file
+        //processInfo.Arguments = @"& {c:\temp\Get-EventLog.ps1}";
+        //execute powershell command
+
+        if (isParent)
+        {
+            processInfo.Arguments = $@"& Send-MailMessage -To '{receiverEmail}' -From 'no-reply@ZMDHKliniek.com' -Subject 'ZMDH intakegesprek bevestiging' -Body 'U kind heeft zich aangemeld voor een intakegesprek op {datum} om {tijd}' -SmtpServer 'localhost' -Port 1025";
+
+        }
+        else
+        {
+            processInfo.Arguments = $@"& Send-MailMessage -To '{receiverEmail}' -From 'no-reply@ZMDHKliniek.com' -Subject 'ZMDH intakegesprek bevestiging' -Body 'Je hebt jezelf aangemeld voor een intakegesprek op {datum} om {tijd}' -SmtpServer 'localhost' -Port 1025";
+
+        }
+
+            //processInfo.Arguments = $@"& Send-MailMessage -To '{receiverEmail}' -From 'no-reply@ZMDHKliniek.com' -Subject 'ZMDH intakegesprek bevestiging' -Body 'Je hebt jezelf aangemeld voor een intakegesprek op {AppointmentDate} met email {receiverEmail}' -SmtpServer 'localhost' -Port 1025";
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.UseShellExecute = false;
+            processInfo.CreateNoWindow = true;
+
+            //start powershell process using process start info
+            Process process = new Process();
+            process.StartInfo = processInfo;
+            process.Start();
+
+            Console.WriteLine("Output - {0}", process.StandardOutput.ReadToEnd());
+            Console.WriteLine("Errors - {0}", process.StandardError.ReadToEnd());
+            process.Close();
+    }
+
+    public string MakeFirstNameCapitalLetter(string firstName) 
+    {
+        if (firstName.Length > 1) { //Execute the following code when the name is longer than 1 character
+            string capitalLetter = firstName.Substring(0, 1);
+            capitalLetter = capitalLetter.ToUpper();
+            string restaint = firstName.Substring(1);
+            restaint = restaint.ToLower();
+            string good = capitalLetter + restaint;
+            
+            return good;
+        } 
+        else 
+        { 
+            return firstName;
+        }
+    }
+
+
     //     BONUS
     //
     // static async Task Execute(string receiverEmail, DateTime AppointmentDate, bool isParent)
@@ -73,7 +142,7 @@ public class AppointmentController : Controller
     //     Console.WriteLine(apiKey);
     //     Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
     //     var client = new SendGridClient(apiKey);
-    //     var from = new EmailAddress("pietsinter90@gmail.com", "ZMDH Kliniek");  //Voer verzender email in
+    //     var from = new EmailAddress("[HIER KOMT SENDGRID EMAIL]]", "ZMDH Kliniek");  //Voer verzender email in
     //     var subject = "ZMDH intakegesprek bevestiging";
     //     var to = new EmailAddress(receiverEmail, "Intakegesprek cliënt");
     //     var plainTextContent = "";

7e5ed32c6eef152bcba255926a3f38224a859692
AhmetAcarer <20119046@student.hhs.nl>
2022-01-11 17:01:49 +0100

    Extra methodes gemaakt
    Co-authored-by: Jamal Saidi <jlsdi@users.noreply.github.com>

--- src/ViewModels/Manage.cs
+++ src/ViewModels/Manage.cs
@@ -13,11 +13,23 @@ public class Manage
         _context = context;
     }
 
-    public async Task CreateChatAsync(Orthopedagogue orthopedagogue)
+    public async Task CreateSelfHelpChatAsync(Orthopedagogue orthopedagogue, string subject, AgeCategory ageCategory)
     {
-        var chat = new Chat() { PrivateChatToken = GetUniquePrivateChatToken(), Orthopedagogue = orthopedagogue};
+        var chat = new Chat() { Orthopedagogue = orthopedagogue, Subject = subject, AgeCategory = ageCategory };
         await _context.Chats.AddAsync(chat);
         await _context.SaveChangesAsync();
+    }
+    \
+    public void DeleteChat(Chat chat)
+    {
+        _context.Chats.Remove(chat);
+    }
+
+    public void DeleteChat(string roomId)
+    {
+        if (!_context.Chats.Any(c => c.RoomId == roomId)) return;
+        var chat = _context.Chats.SingleOrDefault(c => c.RoomId == roomId);
+        _context.Chats.Remove(chat);
     }
 
     public async Task CreateChatAsync(Orthopedagogue orthopedagogue, Client client)

4268b88c5b76dff9e36c0b21de9ee04100330567
Ahmet Acarer <73262708+AhmetAcarer@users.noreply.github.com>
2022-01-17 08:54:02 +0100

    Update Register.cshtml.cs

--- src/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -26,7 +26,7 @@ using WDPR_A.ViewModels;
 
 namespace WDPR_A.Areas.Identity.Pages.Account
 {
-    [Authorize(Roles = "Orthopedagogue")]
+    // [Authorize(Roles = "Orthopedagogue")]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;

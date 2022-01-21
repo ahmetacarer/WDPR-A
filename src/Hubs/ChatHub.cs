cd170b8cbe0f354fb5b28df95ffe2e243334a1e8
AhmetAcarer <20119046@student.hhs.nl>
2022-01-21 16:36:15 +0100

    chat correct bijgewerkt (de groepen zijn functioneel)

--- src/Hubs/ChatHub.cs
+++ src/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using WDPR_A.Models;
 
 namespace WDPR_A.Hubs
@@ -16,14 +17,31 @@ namespace WDPR_A.Hubs
             _context = context;
         }
 
+        public string GetConnectionId() => Context.ConnectionId;
+
+        public Task JoinRoom(string roomId) => Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+        
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+        
+        public async Task RemoveFromGroupIfBlocked(string userId, string roomId)
+        {
+            var client = await _context.Clients.FindAsync(userId);
+            if (client == null || !client.IsBlocked) return;
+            await RemoveFromGroup(roomId);
+        }
+
+
         public async Task SendMessage(string text, string roomId)
         {
             var currentUser = await _userManager.GetUserAsync(Context.User);
             var contextUser = _context.InheritedUsers.SingleOrDefault(u => u.Id == currentUser.Id);
+            await RemoveFromGroupIfBlocked(contextUser.Id, roomId);
             var name = $"{contextUser.FirstName[0]}. {contextUser.LastName}";
             var date = DateTime.Now;
-            // niet de meest efficiente manier, later gerefactored worden naar meerdere methods zoals SendMessageToChat en SendPrivateMessage
-            await Clients.All.SendAsync("ReceiveMessage", name, text, date.ToString("HH:mm"));
+            await Clients.Groups(roomId).SendAsync("ReceiveMessage", name, text, date.ToString("HH:mm"));
             await _context.Messages.AddAsync(new Message { Sender = contextUser, Text = text, When = date, ChatRoomId = roomId });
             await _context.SaveChangesAsync();
         }

ad9ebcbd5d2fae7d7222d9939922aca5c8e82353
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2022-01-23 18:37:18 +0100

    SelfHelpGroup Test aangemaakt

--- test/SelfHelpGroupTest.cs
+++ test/SelfHelpGroupTest.cs
@@ -1,8 +1,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -19,20 +23,18 @@ public class SelfHelpGroupTest
 
 
     [Fact]
-    public void Create_fit_True()
+    public async Task Create_CreateSelfHelpGroup_True()
     {
         var context = ManagerContainer.GetWDPRContext();
-        var userManager = ManagerContainer.TestUserManager<IdentityUser>();
         var generate = ManagerContainer.GetGenerate();
         var chatManager = ManagerContainer.GetChatManager(generate, context);
 
-        var controller = new SelfHelpGroupController(null, context, userManager, chatManager);
 
 
 
         var orthopedagogue = new Orthopedagogue
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "100",
             FirstName = "Lansey",
             LastName = "Jordan",
             Specialty = "Dyslexie",
@@ -40,7 +42,7 @@ public class SelfHelpGroupTest
 
         var client = new Client
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "1",
             FirstName = "Dennis",
             LastName = "Steen",
             Condition = "Dyslexie",
@@ -53,6 +55,35 @@ public class SelfHelpGroupTest
             Email = "dsteen@voorbeeld.nl",
             UserName = "dsteen@voorbeeld.nl",
         };
+
+
+        context.Orthopedagogues.Add(orthopedagogue);
+        context.Clients.Add(client);
+        
+        await context.SaveChangesAsync();
+
+        var principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+                    new Claim(ClaimTypes.NameIdentifier, "100"),
+                    new Claim(ClaimTypes.Role, "Orthopedagogue")
+        }));
+
+        var mockUserStore = new Mock<IUserStore<IdentityUser>>();
+        var mockUserManager = new Mock<UserManager<IdentityUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+        mockUserManager
+            .Setup(_ => _.GetUserAsync(principal))
+            .ReturnsAsync(orthopedagogue);
+
+        var controllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
+
+
+        var sut = new SelfHelpGroupController(null, context, mockUserManager.Object, chatManager);
+        sut.ControllerContext = controllerContext;
+
+        await sut.Create("Dyslexie", AgeCategory.Oudste);
+
+
+        Assert.True(context.Chats.Where(c => !c.IsPrivate).Count() ==  1);
 
 
     }

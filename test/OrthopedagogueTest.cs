26df830e7d8d33ed9a7e47111818fc14aba8e84f
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2022-01-23 19:23:15 +0100

    Tests gemaakt

--- test/OrthopedagogueTest.cs
+++ test/OrthopedagogueTest.cs
@@ -1,123 +1,168 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using WDPR_A.Controllers;
-// using WDPR_A.Models;
-// using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using WDPR_A.Controllers;
+using WDPR_A.Models;
+using Xunit;
 
-// namespace test;
+namespace test;
 
-// public class OrthopedagogueTest
-// {
-//     public WDPRContext GetWDPRContext()
-//     {
-//         var options = new DbContextOptionsBuilder<WDPRContext>().EnableSensitiveDataLogging().
-//                         UseInMemoryDatabase("MijnDatabase")
-//                         .Options;
-//         var context = new WDPRContext(options);
-//         return context;
-//     }
+public class OrthopedagogueTest
+{
+    public WDPRContext GetWDPRContext()
+    {
+        var options = new DbContextOptionsBuilder<WDPRContext>().EnableSensitiveDataLogging().
+                        UseInMemoryDatabase("MijnDatabase")
+                        .Options;
+        var context = new WDPRContext(options);
+        return context;
+    }
 
-//     [Fact]
-//     public async Task OrthopedagogueDashboard_ViewDataTest_AppointmentsFound()
-//     {
-//         var context = GetWDPRContext();
+    [Fact]
+    public async Task OrthopedagogueDashboard_ViewDataTest_AppointmentsFound()
+    {
+        var context = GetWDPRContext();
 
-//         var controller = new OrthopedagogueController(null, context, ManagerContainer.TestUserManager<IdentityUser>());
+        var orthopedagogue = new Orthopedagogue
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = "Jacob",
+            LastName = "Lans",
+            Specialty = "Dyslexie",
+            EmailConfirmed = true
+        };
 
-//         var orthopedagogue = new Orthopedagogue
-//         {
-//             Id = Guid.NewGuid().ToString(),
-//             FirstName = "Jacob",
-//             LastName = "Lans",
-//             Specialty = "Dyslexie",
-//             EmailConfirmed = true
-//         };
+        var client = new Client
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = "Jordy",
+            LastName = "van Dijk",
+            Condition = "Dyslexie",
+            AgeCategory = AgeCategory.Oudste,
+            Guardians = null,
+            Chats = null,
+            Address = "Street 23",
+            Residence = "The Hague",
+            IsBlocked = false,
+            Email = "dsteen@voorbeeld.nl",
+            UserName = "dsteen@voorbeeld.nl",
+            EmailConfirmed = false
+        };
 
-//         var client = new Client
-//         {
-//             Id = Guid.NewGuid().ToString(),
-//             FirstName = "Dennis",
-//             LastName = "Steen",
-//             Condition = "Dyslexie",
-//             AgeCategory = AgeCategory.Oudste,
-//             Guardians = null,
-//             Chats = null,
-//             Address = "Street 23",
-//             Residence = "The Hague",
-//             IsBlocked = false,
-//             Email = "dsteen@voorbeeld.nl",
-//             UserName = "dsteen@voorbeeld.nl",
-//             EmailConfirmed = true
-//         };
+        var appointment = new Appointment
+        {
+            Id = 22,
+            Orthopedagogue = orthopedagogue,
+            IncomingClient = client,
+            IsVerified = false
+        };
 
-//         var appointment = new Appointment
-//         {
-//             Id = 1,
-//             Orthopedagogue = orthopedagogue,
-//             IncomingClient = client,
-//         };
+        context.Orthopedagogues.Add(orthopedagogue);
+        context.Clients.Add(client);
+        context.Appointments.Add(appointment);
+        await context.SaveChangesAsync();
 
-//         context.Orthopedagogues.Add(orthopedagogue);
-//         context.Clients.Add(client);
-//         context.Appointments.Add(appointment);
-//         await context.SaveChangesAsync();
 
-//         var result = await controller.Dashboard();
-//         var viewResult = Assert.IsType<ViewResult>(result);
+        var principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, orthopedagogue.Id),
+        }));
+        var mockUserStore = new Mock<IUserStore<IdentityUser>>();
+        var mockUserManager = new Mock<UserManager<IdentityUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+        mockUserManager
+            .Setup(_ => _.GetUserAsync(principal))
+            .ReturnsAsync(orthopedagogue);
 
-//         Assert.Null(viewResult.ViewData["Melding"]);
+        var controllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
 
-//         var count = Assert.IsType<List<Appointment>>(viewResult.Model).Count;
-//         Assert.True(count == 1);
-//     }
+        var sut = new OrthopedagogueController(null, context, mockUserManager.Object);
+        sut.ControllerContext = controllerContext;
+        var action = await sut.Dashboard();
 
-//     [Fact]
-//     public async Task OrthopedagogueDashboard_ViewDataTest_NoAppointmentsFound()
-//     {
-//         var context = GetWDPRContext();
+        var viewResult = Assert.IsType<ViewResult>(action);
 
-//         var controller = new OrthopedagogueController(null, context, ManagerContainer.TestUserManager<IdentityUser>());
+        Assert.Null(viewResult.ViewData["Melding"]);
 
-//         var orthopedagogue = new Orthopedagogue
-//         {
-//             Id = Guid.NewGuid().ToString(),
-//             FirstName = "Jacob",
-//             LastName = "Lans",
-//             Specialty = "Dyslexie",
-//             EmailConfirmed = true
-//         };
+        var count = Assert.IsType<List<Appointment>>(viewResult.Model).Count;
+        Assert.True(count == 1);
+    }
 
-//         var client = new Client
-//         {
-//             Id = Guid.NewGuid().ToString(),
-//             FirstName = "Dennis",
-//             LastName = "Steen",
-//             Condition = "Dyslexie",
-//             AgeCategory = AgeCategory.Oudste,
-//             Guardians = null,
-//             Chats = null,
-//             Address = "Street 23",
-//             Residence = "The Hague",
-//             IsBlocked = false,
-//             Email = "dsteen@voorbeeld.nl",
-//             UserName = "dsteen@voorbeeld.nl",
-//             EmailConfirmed = false
-//         };
+    [Fact]
+    public async Task OrthopedagogueDashboard_ViewDataTest_NoAppointmentsFound()
+    {
+        var context = GetWDPRContext();
 
-//         context.Orthopedagogues.Add(orthopedagogue);
-//         context.Clients.Add(client);
-//         await context.SaveChangesAsync();
+        var orthopedagogue = new Orthopedagogue
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = "Jacob",
+            LastName = "Lans",
+            Specialty = "Dyslexie",
+            EmailConfirmed = true
+        };
 
-//         var result = await controller.Dashboard();
-//         var viewResult = Assert.IsType<ViewResult>(result);
+        var client = new Client
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = "Dennis",
+            LastName = "Steen",
+            Condition = "Dyslexie",
+            AgeCategory = AgeCategory.Oudste,
+            Guardians = null,
+            Chats = null,
+            Address = "Street 23",
+            Residence = "The Hague",
+            IsBlocked = false,
+            Email = "dsteen@voorbeeld.nl",
+            UserName = "dsteen@voorbeeld.nl",
+            EmailConfirmed = false
+        };
 
-//         Assert.Equal("Er zijn momenteel geen afspraken ingepland.", viewResult.ViewData["Melding"].ToString());
+        var appointment = new Appointment
+        {
+            Id = 23,
+            Orthopedagogue = orthopedagogue,
+            IncomingClient = client,
+            IsVerified = true
+        };
 
-//         var count = Assert.IsType<List<Appointment>>(viewResult.Model).Count;
-//         Assert.True(count == 0);
-//     }
-// }
+        context.Orthopedagogues.Add(orthopedagogue);
+        context.Clients.Add(client);
+        context.Appointments.Add(appointment);
+        await context.SaveChangesAsync();
+
+
+        var principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, orthopedagogue.Id),
+        }));
+        var mockUserStore = new Mock<IUserStore<IdentityUser>>();
+        var mockUserManager = new Mock<UserManager<IdentityUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+        mockUserManager
+            .Setup(_ => _.GetUserAsync(principal))
+            .ReturnsAsync(orthopedagogue);
+
+        var controllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
+
+        var sut = new OrthopedagogueController(null, context, mockUserManager.Object);
+        sut.ControllerContext = controllerContext;
+        var action = await sut.Dashboard();
+
+        var viewResult = Assert.IsType<ViewResult>(action);
+
+        Assert.Equal("Er zijn momenteel geen afspraken ingepland.", viewResult.ViewData["Melding"].ToString());
+
+        var count = Assert.IsType<List<Appointment>>(viewResult.Model).Count;
+        Assert.True(count == 0);
+        Assert.False(count > 0);
+    }
+
+
+    
+}
